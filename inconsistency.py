d5e7243f8c95d26212c4bfd78a4c7f8c99c7b3b7
dishajain1211 <dishajain1211@gmail.com>
2018-06-04 01:58:28 +0530

    Update inconsistency.py

--- inconsistency.py
+++ inconsistency.py
@@ -15,14 +15,47 @@ print (tagset)
 
 tagList = []
 
+soup.select("style")
+tag = soup.style
+type(tag)
+print(tag)
+tag.contents
+
+tagSplit = tag.contents[0].split('}')
+
+AttributeList = []
+mainAttributeList = []
+
+for i in tagSplit[0:13]:
+    f = i.split('{')
+    attributeSplit = f[1].split(';')
+    AttributeList.append(attributeSplit)
+       
+for j in AttributeList:
+    for k in j:
+        if k:
+            if k[0] == ' ':
+                mainAttributeList.append(k[1:])
+            elif k[1] == ' ':
+                mainAttributeList.append(k[2:])
+            else:
+                mainAttributeList.append(k)
+            
 for p in soup.body.find_all():
     if 'style' in p.attrs:
-        print(p.attrs['style'])
+        tempSplit = p.attrs['style'].split(';')
+        for j in tempSplit:
+            if j:
+                if j[0] == ' ':
+                    j = j[1:]
+                if j not in mainAttributeList:
+                    print(j)
+            
 
-for p in soup.body.find_all():
-    if 'style' in p.attrs:
-        print(p)
-
-for p in soup.find_all('p'):
-    if 'style' in p.attrs:
-        del p.attrs['style']
+#for p in soup.body.find_all():
+#    if 'style' in p.attrs:
+#        print(p)
+#
+#for p in soup.find_all('p'):
+#    if 'style' in p.attrs:
+#        del p.attrs['style']
